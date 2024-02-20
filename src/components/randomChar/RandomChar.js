1ac53a6cf3e65d1c68bb04002c5ae11516f48788
dmknpd <soupuabt@gmail.com>
2024-02-20 17:23:45 +0200

    prev commit updated

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -94,13 +94,7 @@ const View = ({ char }) => {
       />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
-        <p className="randomchar__descr">
-          {description
-            ? description.length <= 35
-              ? description
-              : description.substring(0, 35) + "..."
-            : "There is no description for this character"}
-        </p>
+        <p className="randomchar__descr">{description}</p>
         <div className="randomchar__btns">
           <a href={homepage} className="button button__main">
             <div className="inner">homepage</div>
